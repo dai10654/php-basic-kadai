2c24ce6622eee39da516e166fd6f881de0b28d0d
dai10654 <dafuji120@outlook.jp>
2024-08-25 10:54:36 +0900

    kadai_005追加

---
+++ kadai_005/score.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>kadai_005</title>
+</head>
+<body>
+<?php
+
+$score1 = 80;
+$score2 = 60;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 80;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
+// 変数の宣言と値の代入は省略
+
+$sum = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10; // 合計点
+$average = $sum / 10; // 平均点
+
+echo   $average;
+
+?>
+</body>
+</html>
